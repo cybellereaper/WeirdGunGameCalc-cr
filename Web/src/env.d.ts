917a8f64b48e3cf94b53ba455ee2e5f153548c43
cybellereaper <79605349+cybellereaper@users.noreply.github.com>
2026-04-12 02:29:30 +0000

    Delete Web directory

--- Web/src/env.d.ts
+++
@@ -1,7 +0,0 @@
-// src/env.d.ts
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  // Tell TypeScript that any .vue file exports a Vue component
-  const component: DefineComponent<{}, {}, any>
-  export default component
-}
